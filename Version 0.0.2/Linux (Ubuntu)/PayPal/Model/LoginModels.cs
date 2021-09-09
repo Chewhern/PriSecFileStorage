fdc56fc7efbd8f6b172c91cd8168865d7c3df770
Chrono <chrono_legionnaire@protonmail.com>
2021-09-09 11:28:59 +0800

    Add files via upload

---
+++ Version 0.0.2/Linux (Ubuntu)/PayPal/Model/LoginModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriSecFileStorageAPI.Model
+{
+    public class LoginModels
+    {
+        public String RequestStatus { get; set; }
+
+        public String SignedRandomChallengeBase64String { get; set; }
+
+        public String ServerECDSAPKBase64String { get; set; }
+    }
+}
