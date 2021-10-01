3eeafec9a29a02990474e1e0a68d9c1e3bb9861f
Chrono <chrono_legionnaire@protonmail.com>
2021-10-01 23:05:10 +0800

    Add files via upload

---
+++ Version 0.0.3/Linux (Ubuntu)/PayPal/Model/RegisterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriSecFileStorageAPI.Model
+{
+    public class RegisterModel
+    {
+        public String UserIDCount { get; set; }
+        public String UserIDChecker { get; set; }
+        public String Error { get; set; }
+    }
+}
