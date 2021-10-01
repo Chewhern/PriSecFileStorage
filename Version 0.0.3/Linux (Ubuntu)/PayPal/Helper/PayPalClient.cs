9dfd89e2a979be0a11d7b68d430416ee3214b0b0
Chrono <chrono_legionnaire@protonmail.com>
2021-10-01 23:04:18 +0800

    Add files via upload

---
+++ Version 0.0.3/Linux (Ubuntu)/PayPal/Helper/PayPalClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PayPalCheckoutSdk.Core;
+using PayPalHttp;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+namespace PriSecFileStorageAPI.Helper
+{
+    public class PayPalClient
+    {
+        public static PayPalEnvironment environment()
+        {
+            return new SandboxEnvironment("{PayPal Key}", "{PayPal Key}");
+        }
+
+        public static HttpClient client()
+        {
+            return new PayPalHttpClient(environment());
+        }
+
+        public static HttpClient client(string refreshToken)
+        {
+            return new PayPalHttpClient(environment(), refreshToken);
+        }
+
+        public static String ObjectToJSONString(Object serializableObject)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            var writer = JsonReaderWriterFactory.CreateJsonWriter(
+                        memoryStream, Encoding.UTF8, true, true, "  ");
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(serializableObject.GetType(), new DataContractJsonSerializerSettings { UseSimpleDictionaryFormat = true });
+            ser.WriteObject(writer, serializableObject);
+            memoryStream.Position = 0;
+            StreamReader sr = new StreamReader(memoryStream);
+            return sr.ReadToEnd();
+        }
+    }
+}
