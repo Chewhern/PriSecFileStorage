2dd94d2c1234181892a91ec203f365eea506a5ab
Chrono <chrono_legionnaire@protonmail.com>
2021-10-04 19:18:02 +0800

    Add files via upload

---
+++ Version 0.0.4/Linux (Ubuntu)/PayPal/Model/AllowedUserListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriSecFileStorageAPI.Model
+{
+    public class AllowedUserListModel
+    {
+        public String Status { get; set; }
+
+        public String[] AllowedUserID { get; set; }
+    }
+}
