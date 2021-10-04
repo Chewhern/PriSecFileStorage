c5289c162b822c247c6832101815901a50d51e49
Chrono <chrono_legionnaire@protonmail.com>
2021-10-04 19:17:07 +0800

    Add files via upload

---
+++ Version 0.0.4/Linux (Ubuntu)/PayPal/Helper/MyOwnMySQLConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace PriSecFileStorageAPI.Helper
+{
+    public class MyOwnMySQLConnection
+    {
+        public MySqlConnection MyMySQLConnection = new MySqlConnection();
+        public Boolean CheckConnection;
+        public String SecretPath = "{Path to Database Credentials}";
+        public String ConnectionString = "";
+
+        public void setConnection() {
+            using (StreamReader SecretPathReader = new StreamReader(SecretPath))
+            {
+                while ((ConnectionString = SecretPathReader.ReadLine()) != null)
+                {
+                    MyMySQLConnection.ConnectionString = ConnectionString;
+                }
+            }
+        }
+
+        public Boolean LoadConnection(ref String Exception) {
+            setConnection();
+            try
+            {
+                MyMySQLConnection.Open();
+                CheckConnection = true;
+            }
+            catch (MySqlException exception){
+                CheckConnection = false;
+                Exception = exception.ToString();
+            }
+            return CheckConnection;
+        }
+    }
+}
---
+++ Version 0.0.4/Linux (Ubuntu)/PayPal/Helper/PayPalCreateOrder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PayPalCheckoutSdk.Orders;
+using PayPalHttp;
+
+namespace PriSecFileStorageAPI.Helper
+{
+    public class PayPalCreateOrder
+    {
+        private static OrderRequest BuildRequestBody(String CustomInvoiceID, Double TotalPriceValue, Double PriceValue, Double HandlingValue, Double ItemValue, String CustomDescription)
+        {
+            OrderRequest orderRequest = new OrderRequest()
+            {
+                CheckoutPaymentIntent = "CAPTURE",
+
+                ApplicationContext = new ApplicationContext
+                {
+                    BrandName = "Mr Chew IT Software PriSec FileStorage",
+                    LandingPage = "BILLING",
+                    CancelUrl = "https://www.paypal.com",
+                    ReturnUrl = "https://mrchewitsoftware.com.my",
+                    UserAction = "PAY_NOW",
+                },
+                PurchaseUnits = new List<PurchaseUnitRequest>
+                {
+                    new PurchaseUnitRequest{
+                        Description = "Digital Service",
+                        SoftDescriptor = "PriSec Solutions",
+                        InvoiceId = CustomInvoiceID, //Need to put in custom value
+                        AmountWithBreakdown = new AmountWithBreakdown
+                        {
+                            CurrencyCode = "USD",
+                            Value = TotalPriceValue.ToString(), //Need to put in custom value
+                            AmountBreakdown = new AmountBreakdown
+                            {
+                                ItemTotal = new Money
+                                {
+                                    CurrencyCode = "USD",
+                                    Value = PriceValue.ToString() //Need to put in custom value
+                                },
+                                Handling = new Money
+                                {
+                                    CurrencyCode = "USD",
+                                    Value = HandlingValue.ToString() //Need to put in custom value
+                                }
+                            }
+                        },
+                        Items = new List<Item>
+                        {
+                            new Item
+                            {
+                                Name = "File Storage",
+                                Description = CustomDescription, //Custom value need to put in
+                                Sku = "sku01",
+                                UnitAmount = new Money
+                                {
+                                    CurrencyCode = "USD",
+                                    Value = ItemValue.ToString() //Custom value need to put in
+                                },
+                                Quantity = "1",
+                                Category = "DIGITAL_GOODS"
+                            }
+                        },
+                    }
+                }
+            };
+
+            return orderRequest;
+        }
+
+        public async static Task<HttpResponse> CreateOrder(String CustomInvoiceID, Double TotalPriceValue, Double PriceValue, Double HandlingValue, Double ItemValue, String CustomDescription)
+        {
+            var request = new OrdersCreateRequest();
+            request.Headers.Add("prefer", "return=representation");
+            request.RequestBody(BuildRequestBody(CustomInvoiceID, TotalPriceValue, PriceValue, HandlingValue, ItemValue, CustomDescription));
+            var response = await PayPalClient.client().Execute(request);
+
+            return response;
+        }
+    }
+}
